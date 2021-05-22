200f7a8ee9b1a66d09bcf2df451d3bd4621d66f1
gulzhan14 <burgkhankyzygulzhan@gmail.com>
2021-05-22 17:11:46 +0600

    giphy

---
+++ src/Config/index.js
@@ -0,0 +1,2 @@
+export const API = 'http://api.giphy.com/v1/gifs/search?q='
+export const KEY = '&api_key=Xx1CCmQGZfMmCXUu0p5JIkzN2Tx6etqF'
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+  
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import reduxPromise from 'redux-promise'
+import reducer from "./Reducer";
+
+const createStoreWidthMiddleware = applyMiddleware(reduxPromise)(createStore)
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={createStoreWidthMiddleware(reducer)}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
